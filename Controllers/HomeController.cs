aa78ca302337bbd45497a5dd2c3e75e2c79ffd3a
Stuart Williams <stuartw@magenic.com>
2018-08-03 16:14:59 -0700

    Redirect + handy PCF scripts

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace customerAPI.Controllers
+{
+    /// <summary>
+    /// Controller: Home
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Redirect to swagger
+        /// </summary>
+        /// <returns>Swagger page</returns>
+        [Route("")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Redirect("~/swagger");
+        }
+    }
+}
